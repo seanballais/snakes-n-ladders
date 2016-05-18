aaebe9891bee4376081b33446045f0420b4c0df9
Sean Francis N. Ballais <sean@seanballais.com>
2016-05-18 19:37:42 +0800

    Tile jumping fixed.

--- GameProper.java
+++ GameProper.java
@@ -42,39 +42,49 @@ public class GameProper
 			if(ctrForTile1 == ctrForTile2)
 				BoardPanel.tileNo[ctrForTile1].setIcon(new ImageIcon(jpgs[SetPlayerInfo.colorIndexOfPlayers[1]]));
 
-			ctrForTile1 = nextMove(ctrForTile1, 1);
-			BoardPanel.tileNo[ctrForTile1].setIcon(new ImageIcon(jpgs[SetPlayerInfo.colorIndexOfPlayers[0]]));
-		}else{
-
+            int oldTilePosition = ctrForTile1;
+			ctrForTile1 = nextMove(ctrForTile1 + diceNo); // Replace 1 with diceNo
+            if (oldTilePosition + diceNo != ctrForTile1) { // Replace 1 with diceNo
+                JOptionPane.showMessageDialog(null, "Player 1 jumped to tile " + (ctrForTile1 + 1) + ".");
+            }
+            BoardPanel.tileNo[ctrForTile1].setIcon(new ImageIcon(jpgs[SetPlayerInfo.colorIndexOfPlayers[0]]));
+		} else {
 			BoardPanel.tileNo[ctrForTile2].setIcon(null);
 
 			if(ctrForTile1 == ctrForTile2)
 				BoardPanel.tileNo[ctrForTile2].setIcon(new ImageIcon(jpgs[SetPlayerInfo.colorIndexOfPlayers[0]]));
 
-			ctrForTile2 = nextMove(ctrForTile2, 1);
+
+            int oldTilePosition = ctrForTile2;
+    		ctrForTile2 = nextMove(ctrForTile2 + diceNo); // Replace 1 with diceNo
+            if (oldTilePosition + diceNo != ctrForTile2) { // Replace 1 with diceNo
+                JOptionPane.showMessageDialog(null, "Player 2 jumped to tile " + (ctrForTile2 + 1) + ".");
+            }
 			BoardPanel.tileNo[ctrForTile2].setIcon(new ImageIcon(jpgs[SetPlayerInfo.colorIndexOfPlayers[1]]));
 		}
 	}
 
-	public int nextMove(int tile, int diceMove)
+	public int nextMove(int nextTile)
 	{
+        int pairTile = 0;
 		// Checks and moves the piece depending on the tile
-		if (onLadderTile(tile)) {
-			if (getPairTile(tile, BoardPanel.ladderTiles) > tile) {
-				System.out.println("Current tile (ladder): " + tile);
-				System.out.println("Next move (ladder): " + (getPairTile(tile, BoardPanel.ladderTiles)));
-				return getPairTile(tile, BoardPanel.ladderTiles);
+		if (onLadderTile(nextTile)) {
+            pairTile = getPairTile(nextTile, BoardPanel.ladderTiles);
+			if (pairTile > nextTile) {
+				System.out.println("Current tile (ladder): " + nextTile);
+				System.out.println("Next move (ladder): " + pairTile);
+				return pairTile;
 			}
-		} else if (onSnakeTile(tile)) {
-			if (getPairTile(tile, BoardPanel.snakeTiles) < tile) {
-				System.out.println("Current tile (snake): " + tile);
-				System.out.println("Next move (snake): " + (getPairTile(tile, BoardPanel.snakeTiles)));
-				return getPairTile(tile, BoardPanel.snakeTiles);
+		} else if (onSnakeTile(nextTile)) {
+            pairTile = getPairTile(nextTile, BoardPanel.snakeTiles);
+			if (pairTile < nextTile) {
+				System.out.println("Current tile (snake): " + nextTile);
+				System.out.println("Next move (snake): " + pairTile);
+				return pairTile;
 			}
 		}
 
-		System.out.println("Next move: " + diceMove);
-		return tile + diceMove;
+		return nextTile;
 	}
 
 	public boolean onLadderTile(int tile)
